ce0e0eccf9dabcfb9f2a1c3c686e8c1b42e0e82d
Jordan Hayes <hayesjordan@gmail.com>
2014-02-04 14:18:42 -0800

    Added rsync script for sending fastqs to biocluster from hts

---
+++ post_casava/rsync_illumina_data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##################################################
+# Simple script to backup Illumina data to nfs02 #
+##################################################
+
+# Get args
+FCs=$1
+
+# Loop through all flowcells
+for FC in $FCs
+do
+    echo "Starting $FC..."
+    # Sync files over to NFS server
+    rsync -a --progress -e "ssh -i /root/.ssh/genomics_nfs_rsa" /home/www/html/illumina_runs/$FC/ genomics@nfs02.bioinfo.ucr.edu:/pool0/bigdata/genomics/shared/$FC
+    echo "...Transfer Complete"
+done
+
