695db8a4c373839a82dbe1e3410497ea9dc802b8
Jordan Hayes <hayesjordan@gmail.com>
2014-02-13 15:51:25 -0800

    fastqs_rename.R is now compatible with HiSeq (MiSeq is broke)

--- post_casava/fastqs_rename.R
+++ post_casava/fastqs_rename.R
@@ -30,37 +30,50 @@ if (file.exists(args[3])){
 
 # Make sure FASTQ path exists
 if (file.exists(args[4])){
-    fastq_path <- args[4]
+    unaligned_path <- args[4]
 } else{
     stop(paste("FASTQ path ", args[4]," does not exist."))
 }
 
 # Extract needed into
-project <- samples$SampleProject
-samplenumber <- samples$SampleID
-index <- samples$Index
-lane <- samples$Lane
+projects <- samples$SampleProject
 
 # For each pair iterate over samples and rename associated files
 for (p in pairs){
-    for (proj in project){
-        for(i in 1:length(samples[,1])){
-            #file_name <- paste(samplenumber[i],"_.*_L00",lane[i],"_R", p, ".*fastq.gz$", sep="")
-            file_name <- paste('Project_',proj,'/Sample_','',)
+    for (proj in unique(projects)){
+        sample_data <- samples[samples$SampleProject==proj,]
+        sampleids <- sample_data$SampleID        
+        for(sample in sampleids){
+            # Get lane and index for sample
+            lane <- sample_data[sample_data$SampleID==sample,]$Lane
+            index <- sample_data[sample_data$SampleID==sample,]$Index
+
+            # Find files based on file name pattern
+            #file_name <- paste(sample,"_.*_L00",lane[i],"_R", p, ".*fastq.gz$", sep="")
+            fastq_path <- paste(unaligned_path,'/Project_',proj,'/Sample_',sample,'/',sep="")
+            file_name <- paste(sample,"_",index,"_L00",lane[i],"_R", p, ".*fastq.gz$", sep="")
             files <- list.files(path=fastq_path,pattern=file_name)
+
+            # If we found some files, create symlinks
             if (length(files) > 0){
                 if (!is.na(index)){
-                    commands <- paste("ln -s ",fastq_path,'/',files, "  ", flowcell, "_lane",lane[i],"_pair",p,"_",index[i],".fastq.gz",sep="")
+                    commands <- paste("ln -s ",fastq_path,'/',files, "  ", flowcell, "_lane",lane[i],"_pair",p,"_",index,".fastq.gz",sep="")
                 }else{
                     commands <- paste("ln -s ",fastq_path,'/',files, "  ", flowcell, "_lane",lane[i],"_pair",p,".fastq.gz",sep="")
                 }
                 print(commands)
                 system(commands)
             } else{
-               print(paste("WARNING:: No files matching pattern ",file_name))
+               warning(paste("WARNING:: No files matching pattern ",file_name))
             }
         }
     }
 }
+
+# Print first 50 warnings
+#if (length(warnings) > 0){
+#    warnings()
+#}
+
 # Create QC sym-links
 # Unaligned/Basecall_Stats_
