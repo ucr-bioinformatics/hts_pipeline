4c5d82ad1501c2ae73d08c94bac029c5efd6f142
Jordan Hayes <hayesjordan@gmail.com>
2014-05-01 17:01:46 -0700

    Added email notification

--- bin/miseq_start.sh
+++ bin/miseq_start.sh
@@ -4,6 +4,9 @@
 # Check for MiSeq data and start pipeline #
 ###########################################
 
+# Make sure there is only one version of this running
+lockfile -r 0 /tmp/miseq_start.lock || exit 1
+
 # Check Arguments
 EXPECTED_ARGS=1
 E_BADARGS=65
@@ -18,6 +21,7 @@ SOURCE_DIR=$1
 
 # Change directory to source
 cd $SOURCE_DIR
+lockfile -r 0 miseq_start.lock || exit 1
 
 # Get list of Run directories
 dir_list=`find . -maxdepth 1 -type d`
@@ -30,8 +34,25 @@ for dir in $dir_list; do
         if [ ! -z $sample_sheet ]; then
             # Determine flowcell ID
             FC_ID=`echo $dir | cut -dl -f4`
+
+            # Send email notification
+            /usr/sbin/sendmail -vt << EOF
+To: hts@biocluster.ucr.edu
+CC: jhayes@ucr.edu
+From: no-reply@biocluster.ucr.edu
+Subject: HTS Pipeline: Flowcell $FC_ID: Started
+
+Flowcell $FC_ID has come in and needs to be processed.
+Thanks
+EOF
+
             # Transfer miseq data
             rsync_miseq_data.sh $FC_ID $SOURCE_DIR/$dir
         fi
     fi
 done
+
+# Remove lock files
+rm -f miseq_start.lock
+rm -f /tmp/miseq_start.lock
+
