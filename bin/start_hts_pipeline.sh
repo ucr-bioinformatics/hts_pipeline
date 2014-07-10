21bf656a630263b1095f1801849c09d1d7106f4c
Jordan Hayes <hayesjordan@gmail.com>
2014-07-09 17:35:37 -0700

    Added Torque email notifications

--- bin/start_hts_pipeline.sh
+++ bin/start_hts_pipeline.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -l
 touch $HTS_PIPELINE_HOME/log/miseq_pipeline.log
-echo "miseq_start.sh /bigdata/cclark/" | qsub -l nodes=1:ppn=8,mem=10gb
+echo "miseq_start.sh /bigdata/cclark/" | qsub -l nodes=1:ppn=8,mem=10gb -m aeb -M hts@biocluster.ucr.edu
