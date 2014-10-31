23e56f960ba8757dd9ea382d651858d50cacbcc1
Jordan Hayes <hayesjordan@gmail.com>
2014-10-31 11:37:53 -0700

    Added check for I files when index == NA

--- post_casava/bin/fastqs_rename.R
+++ post_casava/bin/fastqs_rename.R
@@ -113,7 +113,12 @@ gen_link <- function(x) {
                     commands <- paste("ln -s ",ufastq_path,'/',f, "  ", "Undetermined_lane",lane,"_pair",p,".fastq.gz",sep="")
                 } else if ( i==2 ) {
                     # Processes Index files (I1,I2,etc...)
-                    commands <- paste("ln -s ",fastq_path,'/',f, "  ", flowcell, "_lane",lane,"_pair",index_pair_num,"_",index,".fastq.gz",sep="")
+                    if (!is.na(index)) {
+                        commands <- paste("ln -s ",fastq_path,'/',f, "  ", flowcell, "_lane",lane,"_pair",index_pair_num,"_",index,".fastq.gz",sep="")
+                    }
+                    else {
+                        commands <- paste("ln -s ",fastq_path,'/',f, "  ", flowcell, "_lane",lane,"_pair",index_pair_num,".fastq.gz",sep="")
+                    }
                 } else if ( i==1 ) {
                     # Processes sequence files (R1,R2,etc...)
                     if (!is.na(index)) {
