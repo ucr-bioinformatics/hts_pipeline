1c0947130514eb6997c8b98520e79dfcb3b024b1
Jordan Hayes <jhayes@ucr.edu>
2015-09-28 12:25:32 -0700

    Added --use-bases-mask option to bcl2fastq wrapper script

--- casava/bin/bcl2fastq_run.sh
+++ casava/bin/bcl2fastq_run.sh
@@ -1,22 +1,28 @@
 #!/bin/bash -l
 
 # Check Arguments
-EXPECTED_ARGS=2
+EXPECTED_ARGS=3
 E_BADARGS=65
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
-  echo "Usage: `basename $0` {FlowcellID} {RunDirectoryName}"
+  echo "Usage: `basename $0` {FlowcellID} {RunDirectoryName} {BaseMask}"
   exit $E_BADARGS
 fi
 
 # Get args
 fc_id=$1
 run_dir=$2
+base_mask=$3
 
 module load bcl2fastq
 cd $SHARED_GENOMICS/RunAnalysis/flowcell$fc_id
 echo "Creating output directory..."
 mkdir -p $SHARED_GENOMICS/$fc_id/$run_dir
 echo "Running bcl2fastq for demultiplexing..."
-nohup bcl2fastq --runfolder-dir=$run_dir --processing-threads=64 --demultiplexing-threads=12 --loading-threads=4 --writing-threads=4 --output-dir=$SHARED_GENOMICS/$fc_id/$run_dir > $SHARED_GENOMICS/$fc_id/nohup.out &
+if [[ ! $base_mask == "NA" ]]; then
+    nohup bcl2fastq --use-bases-mask=$base_mask --runfolder-dir=$run_dir --processing-threads=64 --demultiplexing-threads=12 --loading-threads=4 --writing-threads=4 --output-dir=$SHARED_GENOMICS/$fc_id/$run_dir > $SHARED_GENOMICS/$fc_id/nohup.out &
+else
+    nohup bcl2fastq --runfolder-dir=$run_dir --processing-threads=64 --demultiplexing-threads=12 --loading-threads=4 --writing-threads=4 --output-dir=$SHARED_GENOMICS/$fc_id/$run_dir > $SHARED_GENOMICS/$fc_id/nohup.out &
+fi
+
