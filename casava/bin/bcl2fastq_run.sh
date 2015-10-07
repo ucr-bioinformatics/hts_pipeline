1b19ff37b8c2625a23d4b6aa29947096f9595123
Neerja Katiyar <neerjabiotechinfo@gmail.com>
2015-10-07 12:09:14 -0700

    Edited bcl2fastq, samplesheet option working now

--- casava/bin/bcl2fastq_run.sh
+++ casava/bin/bcl2fastq_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
 # Check Arguments
-EXPECTED_ARGS=3
+EXPECTED_ARGS=4
 E_BADARGS=65
 
 if [ $# -ne $EXPECTED_ARGS ]
@@ -14,7 +14,7 @@ fi
 fc_id=$1
 run_dir=$2
 base_mask=$3
-samplesheet=$4
+sample_sheet=$4
 
 module load bcl2fastq
 cd $SHARED_GENOMICS/RunAnalysis/flowcell$fc_id
@@ -24,9 +24,9 @@ echo "Running bcl2fastq for demultiplexing..."
 
 if [[ ! "${base_mask}" == "NA" ]]; then
     echo -e "\tRunning with BaseMask ${base_mask}"
-    bcl2fastq --use-bases-mask=$base_mask --runfolder-dir=$run_dir --sample-sheet=$samplesheet --processing-threads=64 --demultiplexing-threads=12 --loading-threads=4 --writing-threads=4 --output-dir=$SHARED_GENOMICS/$fc_id/$run_dir 2>&1 | tee $SHARED_GENOMICS/$fc_id/nohup.out
+    bcl2fastq --use-bases-mask=$base_mask --runfolder-dir=$run_dir --sample-sheet=$sample_sheet --processing-threads=64 --demultiplexing-threads=12 --loading-threads=4 --writing-threads=4 --output-dir=$SHARED_GENOMICS/$fc_id/$run_dir 2>&1 | tee $SHARED_GENOMICS/$fc_id/nohup.out
 else
     echo -e "\tRunning with out BaseMask"
-    bcl2fastq --runfolder-dir=$run_dir --sample-sheet=$samplesheet --processing-threads=64 --demultiplexing-threads=12 --loading-threads=4 --writing-threads=4 --output-dir=$SHARED_GENOMICS/$fc_id/$run_dir 2>&1 | tee $SHARED_GENOMICS/$fc_id/nohup.out
+    bcl2fastq --runfolder-dir=$run_dir --sample-sheet=$sample_sheet --processing-threads=64 --demultiplexing-threads=12 --loading-threads=4 --writing-threads=4 --output-dir=$SHARED_GENOMICS/$fc_id/$run_dir 2>&1 | tee $SHARED_GENOMICS/$fc_id/nohup.out
 fi
 
