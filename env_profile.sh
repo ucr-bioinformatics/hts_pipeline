87fce183e9be68a3577dfb11680434814f54539d
Jordan Hayes <jhayes@ucr.edu>
2016-03-21 10:39:00 -0700

    Modified env_profile to auto generate values based on user

--- env_profile.sh
+++ env_profile.sh
@@ -7,8 +7,22 @@ else
     echo "Please set your HTS_PIPELINE_HOME environment variable"
 fi
 
-# Set genomics data path
-export SHARED_GENOMICS=/bigdata/bioinfo/jhayes/genomics_shared
+if [[ $USER -eq genomics ]]; then
+    # Set genomics data path
+    export SHARED_GENOMICS=/bigdata/genomics/shared
 
-# Send email notifications to this address
-export NOTIFY_EMAIL=jhayes@ucr.edu
+    # Send email notifications to this address
+    export NOTIFY_EMAIL=hts@biocluster.ucr.edu
+else
+    # Set genomics data path
+    GROUP=$(id -g -n)
+    export SHARED_GENOMICS=/bigdata/$GROUP/$USER/genomics_shared
+    if [[ ! -f $SHARED_GENOMICS ]]; then
+        mkdir -p $SHARED_GENOMICS/Runs
+        mkdir -p $SHARED_GENOMICS/RunAnalysis
+    fi
+
+    # Send email notifications to this address
+    USER_NAME=$(getent passwd|grep $USER|cut -d: -f5 |tr ' ' '.'|awk '{print tolower($0)}')
+    export NOTIFY_EMAIL=$USER_NAME@ucr.edu
+fi
