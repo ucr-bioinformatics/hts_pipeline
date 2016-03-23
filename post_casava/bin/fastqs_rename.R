17ac4b881e4d48cd643ff8b9774e7d5e32e3733a
Siddhartha Kanrar <skanrar@pigeon.ib.int.bioinfo.ucr.edu>
2016-03-23 16:14:25 -0700

    Enable qc-link for MiSeq

--- post_casava/bin/fastqs_rename.R
+++ post_casava/bin/fastqs_rename.R
@@ -146,16 +146,14 @@ if (length(warn) > 0){
     print(warn)
 }
 
-if(run_type == "nextseq" || run_type == "hiseq"){
 # Create QC sym-link
-    qcs <- list.files(path=unaligned_path, pattern='^qc[0-9]*$')
-    if (length(qcs) > 1) {
-            last_qc <- qcs[-1]
-            next_qc <- paste('qc', is.numeric(gsub("^qc", '',last_qc))+1, sep="")
-    }else {
-            next_qc <- 'qc'
-    }
-    system(paste('ln -s ', unaligned_path, '/Reports/html/ ', next_qc, sep=""))
+qcs <- list.files(path=unaligned_path, pattern='^qc[0-9]*$')
+if (length(qcs) > 1) {
+    last_qc <- qcs[-1]
+    next_qc <- paste('qc', is.numeric(gsub("^qc", '',last_qc))+1, sep="")
+}else {
+    next_qc <- 'qc'
 }
+system(paste('ln -s ', unaligned_path, '/Reports/html/ ', next_qc, sep=""))
 
 print("Rename complete")
