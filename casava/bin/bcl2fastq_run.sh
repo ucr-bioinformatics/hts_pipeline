05cdc4a057447a07fc1c3e2e86f12bd9aa236cb4
Neerja Katiyar <neerjabiotechinfo@gmail.com>
2016-09-02 15:18:14 -0700

    Added the argument for no-lane-splitting in bcl2fastq

--- casava/bin/bcl2fastq_run.sh
+++ casava/bin/bcl2fastq_run.sh
@@ -6,7 +6,7 @@ E_BADARGS=65
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
-  echo "Usage: `basename $0` {FlowcellID} {RunDirectoryName} {BaseMask} {SampleSheet} {Mismatch, default=1}"
+  echo "Usage: `basename $0` {FlowcellID} {RunDirectoryName} {BaseMask} {SampleSheet} {Mismatch, default=1} {NoSplit}"
   exit $E_BADARGS
 fi
 
@@ -17,6 +17,12 @@ base_mask=$3
 sample_sheet=$4
 barcode_mismatch=$5
 
+if [[ $6 == "nosplit" ]]; then
+    lane_split="--no-lane-splitting"
+else
+    lane_split=""
+fi
+    
 module load bcl2fastq
 cd $SHARED_GENOMICS/RunAnalysis/flowcell$fc_id
 echo "Creating output directory..."
@@ -25,9 +31,9 @@ echo "Running bcl2fastq for demultiplexing..."
 
 if [[ ! "${base_mask}" == "NA" ]]; then
     echo -e "\tRunning with BaseMask ${base_mask}"
-    bcl2fastq --use-bases-mask=$base_mask --runfolder-dir=$run_dir --sample-sheet=$sample_sheet --barcode-mismatches=$barcode_mismatch --minimum-trimmed-read-length 0 --mask-short-adapter-reads 0 --ignore-missing-bcls --ignore-missing-filter --ignore-missing-positions --ignore-missing-controls --processing-threads=64 --demultiplexing-threads=12 --loading-threads=4 --writing-threads=4 --output-dir=$SHARED_GENOMICS/$fc_id/$run_dir 2>&1 | tee $SHARED_GENOMICS/$fc_id/nohup.out
+    bcl2fastq --use-bases-mask=$base_mask --runfolder-dir=$run_dir --sample-sheet=$sample_sheet --barcode-mismatches=$barcode_mismatch --minimum-trimmed-read-length 0 --mask-short-adapter-reads 0 --ignore-missing-bcls --ignore-missing-filter --ignore-missing-positions --ignore-missing-controls $lane_split --processing-threads=64 --demultiplexing-threads=12 --loading-threads=4 --writing-threads=4 --output-dir=$SHARED_GENOMICS/$fc_id/$run_dir 2>&1 | tee $SHARED_GENOMICS/$fc_id/nohup.out
 else
     echo -e "\tRunning with out BaseMask"
-    bcl2fastq --runfolder-dir=$run_dir --sample-sheet=$sample_sheet --barcode-mismatches=$barcode_mismatch --ignore-missing-bcls --ignore-missing-filter --ignore-missing-positions --ignore-missing-controls --processing-threads=64 --demultiplexing-threads=12 --loading-threads=4 --writing-threads=4 --output-dir=$SHARED_GENOMICS/$fc_id/$run_dir 2>&1 | tee $SHARED_GENOMICS/$fc_id/nohup.out
+    bcl2fastq --runfolder-dir=$run_dir --sample-sheet=$sample_sheet --barcode-mismatches=$barcode_mismatch --ignore-missing-bcls --ignore-missing-filter --ignore-missing-positions --ignore-missing-controls $lane_split --processing-threads=64 --demultiplexing-threads=12 --loading-threads=4 --writing-threads=4 --output-dir=$SHARED_GENOMICS/$fc_id/$run_dir 2>&1 | tee $SHARED_GENOMICS/$fc_id/nohup.out
 fi
 
