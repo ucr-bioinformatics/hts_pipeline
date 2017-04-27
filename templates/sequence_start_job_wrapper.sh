31aef13b5680e906813575fe3043aeadeee733cd
Neerja Katiyar <neerjabiotechinfo@gmail.com>
2017-04-27 15:58:29 -0700

    Changed queue for job submission to short

--- templates/sequence_start_job_wrapper.sh
+++ templates/sequence_start_job_wrapper.sh
@@ -3,10 +3,10 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=10
 #SBATCH --mem-per-cpu=50G
-#SBATCH --time=20:00:00
+#SBATCH --time=2:00:00
 #SBATCH --mail-user=${7}
 #SBATCH --mail-type=ALL
-#SBATCH -p batch
+#SBATCH -p short
 
 sleep $(($RANDOM % 10))
 ${1}_start.sh ${2} ${3}/${4} ${1} ${5}
