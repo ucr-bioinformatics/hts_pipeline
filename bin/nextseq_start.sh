72583361e840a4f019fb69db88cadba339cf5575
William Shiao <wshia002@ucr.edu>
2017-06-23 12:50:22 -0700

    Add QC report #2 commands to error.log

--- bin/nextseq_start.sh
+++ bin/nextseq_start.sh
@@ -86,7 +86,7 @@ if [ -f $complete_file ]; then
     
     # Set dual_index_flag by checking SampleSheet
     if grep -qE 'I7_Index_ID.*I5_Index_ID|I5_Index_ID.*I7_Index_ID' ${SHARED_GENOMICS}/RunAnalysis/flowcell${FC_ID}/$run_dir/SampleSheet.csv; then
-    	dual_index_flag=1
+        dual_index_flag=1
     fi
 
     # Create Sample Sheet for demux
@@ -209,14 +209,24 @@ if [ -f $complete_file ]; then
     fi
 
     # Generate second QC report
-    ls $SHARED_GENOMICS/$FC_ID/*.fastq.gz > $SHARED_GENOMICS/$FC_ID/file_list_new.txt
-
+    CMD="ls $SHARED_GENOMICS/$FC_ID/*.fastq.gz > $SHARED_GENOMICS/$FC_ID/file_list_new.txt"
+    echo -e "==== SECOND QC STEP ====\n${CMD}" >> $ERROR_FILE
+    ${CMD} 
+    if [ $? -ne 0 ]; then
+        echo "ERROR: Failed to generate file list" >> $ERROR_FILE && ERROR=1
+    fi
+ 
     while IFS= read -r file
     do
         [ -f "$file" ]
         module load slurm
-        sbatch generate_qc_report_wrapper.sh ${SHARED_GENOMICS} ${FC_ID} ${file} 0
-        #echo "module load fastqc; fastqc -o $SHARED_GENOMICS/$FC_ID/fastq_report/ $file" | qsub -lnodes=1:ppn=4,mem=16g,walltime=4:00:00;
+        # echo "module load fastqc; fastqc -o $SHARED_GENOMICS/$FC_ID/fastq_report/ $file" | qsub -lnodes=1:ppn=4,mem=16g,walltime=4:00:00;
+        CMD="sbatch generate_qc_report_wrapper.sh ${SHARED_GENOMICS} ${FC_ID} ${file} 0"
+        echo "${CMD}" >> $ERROR_FILE
+        ${CMD} &>> $ERROR_FILE
+        if [ $? -ne 0 ]; then
+            echo "ERROR: Failed to add QC report generation #2 to slurm queue" && ERROR=1
+        fi
     done < "file_list_new.txt"
 
     # Update Illumina web server URLs
@@ -226,7 +236,7 @@ if [ -f $complete_file ]; then
         ${CMD} &>> $ERROR_FILE
         if [ $? -ne 0 ]; then
             echo "ERROR: Illumina URL update failed" >> $ERROR_FILE && ERROR=1
-        fi
+        fi  
     fi
 
     # Remove lock files
