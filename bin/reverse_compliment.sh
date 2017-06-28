0c30e4430b44aef92321a6a0950efcac3f760429
William Shiao <wshia002@ucr.edu>
2017-06-28 13:41:54 -0700

    Clean up reverse_compliment.sh

--- bin/reverse_compliment.sh
+++ bin/reverse_compliment.sh
@@ -1,22 +1,22 @@
 #!/bin/bash -l
 
-source $HTS_PIPELINE_HOME/env_profile.sh
+source "$HTS_PIPELINE_HOME/env_profile.s"h
 
 if (($# < 1)); then
     echo "Usage: reverse_compliment.sh <SampleSheet> [barcodeColumn = 6] [printOnly = 0]"
     exit 1
 fi
 
-if (($# == 1)); then
-    COLUMN_NUM=6
-else
-    COLUMN_NUM=$2
-fi
-
 SAMPLESHEET="$1"
 TMP_SAMPLESHEET="$1.tmp"
 
-awk -F',' -v colNum="$COLUMN_NUM" '
+# Handle the case of $SAMPLESHEET.tmp already existing by appending another .tmp
+while [ -f "$TMP_SAMPLESHEET" ]; do
+    TMP_SAMPLESHEET="${TMP_SAMPLESHEET}.tmp"
+done
+    
+
+awk -F',' -v colNum="${COLUMN_NUM:-6}" '
 BEGIN {
   OFS = FS;
 }
@@ -29,13 +29,13 @@ BEGIN {
   "echo "$colNum" | rev | tr ATCG TAGC" | getline newcode;
   $colNum=newcode
   print $0
-}' $SAMPLESHEET > $TMP_SAMPLESHEET
+}' "$SAMPLESHEET" > "$TMP_SAMPLESHEET"
 
 if [ "$3" == "1" ]; then
     cat "$TMP_SAMPLESHEET"
     rm -f "$TMP_SAMPLESHEET"
 else
-    rm -f $SAMPLESHEET
-    mv $TMP_SAMPLESHEET $SAMPLESHEET
+    rm -f "$SAMPLESHEET"
+    mv "$TMP_SAMPLESHEET" "$SAMPLESHEET"
 fi
 
