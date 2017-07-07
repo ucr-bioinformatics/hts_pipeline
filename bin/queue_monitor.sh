a9633aa35d0c2cf0684e1d3d56dd1fd47706bef2
William Shiao <wshia002@ucr.edu>
2017-07-07 14:42:38 -0700

    Add script to monitor slurm queue

---
+++ bin/queue_monitor.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ $# -lt 2  ]]; then
+    echo "Usage: queue_monitor.sh <queue_user> <alert_email>"
+    exit 1
+fi
+
+COMMAND="squeue -u $1"
+
+while [ $($COMMAND | wc -l) -gt 1 ]; do
+    $COMMAND
+    sleep 30
+done
+
+if [[ ! -z "$2" ]]; then
+    echo "Sending email to $2"
+    /usr/sbin/sendmail -t << EOF
+To: $2
+From: no-reply@biocluster.ucr.edu
+Subject: Queue Completed
+
+The queue for user $1 is now empty.
+
+EOF
+fi
+
