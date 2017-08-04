ebe471f2579640a8c16494af1b3c4e5d33ca36dd
William Shiao <wshia002@ucr.edu>
2017-08-04 16:12:34 -0700

    Fix incorrect quoting in hiseq_start.sh

--- bin/hiseq_start.sh
+++ bin/hiseq_start.sh
@@ -275,7 +275,7 @@ if [ -f $complete_file ]; then
     #fi
     
     # We demux
-    CMD='bcl2fastq_run.sh ${FC_ID} $run_dir "${BASEMASK}" ${SHARED_GENOMICS}/${FC_ID}/SampleSheet.csv 1 ""'
+    CMD="bcl2fastq_run.sh ${FC_ID} $run_dir "${BASEMASK}" ${SHARED_GENOMICS}/${FC_ID}/SampleSheet.csv 1 ''"
     echo -e "==== DEMUX STEP ====\n${CMD}" >> $ERROR_FILE
     if [ $ERROR -eq 0 ]; then
         cd $SHARED_GENOMICS/$FC_ID
--- post_casava/bin/send_flowcell_email.sh
+++ post_casava/bin/send_flowcell_email.sh
@@ -5,18 +5,53 @@ if [[ $# -ne 1 ]]; then
     exit 1
 fi
 
+FC_ID="$1"
+
 # Load DB passwords
 source "$HTS_PIPELINE_HOME/bin/load_hts_passwords.sh"
 
 # Email information (constants)
 EMAIL_SENDER="William Shiao <wshia002@ucr.edu>"
-EMAIL_CC="${EMAIL_SENDER}, Clay Clark <clay.clark@ucr.edu>, Glenn Hicks <glenn.hicks@ucr.edu>, Neerja Katiyar <neerja.katiyar@ucr.edu>"
+#EMAIL_CC="${EMAIL_SENDER}, Clay Clark <clay.clark@ucr.edu>, Glenn Hicks <glenn.hicks@ucr.edu>, Neerja Katiyar <neerja.katiyar@ucr.edu>"
+EMAIL_CC="${EMAIL_SENDER}"
 
-# TODO: Populate variables with information from DB
-FC_ID="$1"
+TARGET_DIR="${SHARED_GENOMICS}/${FC_ID}"
+dir_list=$(find "$TARGET_DIR" -maxdepth 1 -type d)
+
+for dir in $dir_list; do
+    str=$(echo "$dir" | rev | cut -d / -f 1 | rev | cut -d_ -f2)
+    echo "testing $dir -> $str"
+    case $str in
+		"SN279")
+			SEQ="HiSeq"
+		;;
+		"NB501124")
+			SEQ="NextSeq"
+            ;;
+		"NB501891")
+			SEQ="NextSeq"
+		;;
+		"M02457")
+			SEQ="MiSeq"
+		;;
+		*) 
+		;;
+	esac
+done
+
+echo "Sequencer: ${SEQ}"
+
+# Populate variables with information from DB
 FC_EMAIL=$(mysql -D projects -h illumina.int.bioinfo.ucr.edu -u $DB_USERNAME -p $DB_PASSWORD -N -s -e "SELECT email FROM flowcell_list INNER JOIN project_list ON project_list.project_id = flowcell_list.lane_1_project WHERE flowcell_id=${FC_ID}")
+echo "mysql -D projects -h illumina.int.bioinfo.ucr.edu -u $DB_USERNAME -p $DB_PASSWORD"
+
+if [[ -z "$FC_EMAIL" ]]; then
+    echo "Got empty target email... exiting."
+    exit 3
+fi
 
 echo "Sending email for flowcell #${FC_ID} to ${FC_EMAIL}"
+
 
 MAIL_HEADERS=$(cat <<EOF
 From: $EMAIL_SENDER
@@ -56,6 +91,9 @@ if [[ $SEQ == "NextSeq" ]]; then
     EMAIL_BODY="$NEXTSEQ_EMAIL"
 elif [[ $SEQ == "MiSeq" ]]; then
     EMAIL_BODY="$MISEQ_EMAIL"
+else
+    echo "Unknown sequencer... exiting."
+    exit 2
 fi
 
 echo "Sending email to $FC_USER"
