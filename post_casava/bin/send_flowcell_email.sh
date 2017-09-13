3994e75f2c136f993c5e513ad4dc0fe3064e6fc2
William Shiao <wshia002@ucr.edu>
2017-09-13 13:24:46 -0700

    Fix adding a note to send email script

--- post_casava/bin/send_flowcell_email.sh
+++ post_casava/bin/send_flowcell_email.sh
@@ -52,8 +52,8 @@ if [[ $# -ge 3 ]]; then
     FC_EMAIL="${FC_EMAIL},${3}"
 fi
 
-if [[ $# -ge 4 ]]; then
-    NOTE=$"\n$4\n"
+if [[ $# -ge 4 ]] && [[ ! -z "$4" ]]; then
+    NOTE=$'\n$4\n'
 fi
 
 
@@ -100,7 +100,8 @@ else
 fi
 
 echo "Sending email for flowcell #${FC_ID} to ${FC_EMAIL}"
-echo -e "Email contents: \n${EMAIL_BODY}"
+echo -e "Email contents:"
+echo "${EMAIL_BODY}"
 
 if [[ $# -lt 2 ]] || [[ $# -ge 2 && $2 -eq 1 ]]; then
 	read -p "Are you sure? " -n 1 -r
