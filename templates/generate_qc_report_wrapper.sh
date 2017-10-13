f860a338276be9a6bb019218b3b4752e60dbcd82
William Shiao <wshia002@ucr.edu>
2017-10-13 13:19:41 -0700

    Increase time and cpu count for fastQC report

--- templates/generate_qc_report_wrapper.sh
+++ templates/generate_qc_report_wrapper.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 
 #SBATCH --nodes=1
-#SBATCH --ntasks=10
+#SBATCH --ntasks=16
 #SBATCH --mem-per-cpu=4G
-#SBATCH --time=2:00:00
+#SBATCH --time=4:00:00
 #SBATCH --mail-user=${7}
 #SBATCH --mail-type=ALL
-#SBATCH -p short 
+#SBATCH -p intel 
 
 FILES="$4"
 
@@ -14,7 +14,7 @@ echo "Generating QC report for ${FILES}"
 
 module load fastqc
 if [ "${3}" == "0" ]; then
-    fastqc -t 10 -o ${1}/${2}/fastq_report/ ${FILES}
+    fastqc -t 16 -o ${1}/${2}/fastq_report/ ${FILES}
 else
     fastqc -o ${1}/${2}/fastq_report/fastq_report_lane${3}/ ${FILES}
 fi
