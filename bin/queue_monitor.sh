23f581c81e2b94ab73dece9125e7baa850a2aab5
Jordan Hayes <hayesjordan@gmail.com>
2018-01-10 14:44:16 -0800

    Now used to notify Genomics Core, instead of developers

--- bin/queue_monitor.sh
+++ bin/queue_monitor.sh
@@ -5,23 +5,29 @@ if [[ $# -lt 2  ]]; then
     exit 1
 fi
 
-COMMAND="squeue -u $1"
+# For deamon mode only
+#COMMAND="squeue -u $1"
+#while [ $($| wc -l) -gt 1 ]; do
+#    $COMMAND
+#    sleep 30
+#done
 
-while [ $($COMMAND | wc -l) -gt 1 ]; do
-    $COMMAND
-    sleep 30
-done
+NUM_JOBS=$(squeue -u $1 --noheader | wc -l)
+RUNNING_JOBS=$(squeue -t R -u $1 --noheader | wc -l)
+PENDING_JOBS=$(squeue -t PD -u $1 --noheader --start -l)
 
-if [[ ! -z "$2" ]]; then
+if [[ ! -z "$2" && ${NUM_JOBS} -gt 0 ]]; then
     echo "Sending email to $2"
     /usr/sbin/sendmail -t << EOF
 To: $2
 From: no-reply@biocluster.ucr.edu
-Subject: Queue Completed
+Subject: HTS Pipeline Status
 
-The queue for user $1 is now empty.
+Total number of jobs: $NUM_JOBS
+Number of running jobs: $RUNNING_JOBS
+Status of pending jobs:
+$PENDING_JOBS
 
 EOF
     exit $?
 fi
-
