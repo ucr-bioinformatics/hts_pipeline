62bf15d371dec3d144616f02cbb8da3499501c48
Jordan Hayes <hayesjordan@gmail.com>
2018-01-11 14:23:37 -0800

    Hard coded job emails to go to Clay

--- templates/sequence_start_job_wrapper.sh
+++ templates/sequence_start_job_wrapper.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=10
 #SBATCH --mem-per-cpu=5G
 #SBATCH --time=4:00:00
-#SBATCH --mail-user=${7}
+#SBATCH --mail-user="clay.clark@ucr.edu"
 #SBATCH --mail-type=ALL
 #SBATCH -p batch
 
